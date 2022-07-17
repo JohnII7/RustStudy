4cdf11c68bb93200e0be0e1c3834fc56bb0e4f3f
JohnII7 <157798378@qq.com>
2022-07-18 00:43:53 +0800

    rust的loop和异常处理

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,25 @@ use rand::Rng;
 fn main() {
     println!("猜个数字");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("神秘数字是{}", secret_number);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法执行");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法执行");
 
-    // shadow
-    let guess:u32 = guess.trim().parse().expect("Please type a number");
+        // shadow
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
-    println!("你猜的是: {}", guess);
+        println!("你猜的是: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less =>println!("Too small !"),
-        Ordering::Greater=>println!("Too big!"),
-        Ordering::Equal=>println!("You win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small !"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
