cbd326944ae5157273e06fcdb35b5e45689cf94b
JohnII7 <157798378@qq.com>
2022-07-17 19:43:41 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::io; //prelude
-use rand::Rng; //trait
+use std::cmp::Ordering;
+use std::io;
+//prelude
+use rand::Rng;
+
+//trait
 fn main() {
     println!("猜个数字");
-    let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("神秘数字是{}",secret_number);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("神秘数字是{}", secret_number);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("无法执行");
-    println!("你猜的是: {}",guess);
+
+    // shadow
+    let guess:u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("你猜的是: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less =>println!("Too small !"),
+        Ordering::Greater=>println!("Too big!"),
+        Ordering::Equal=>println!("You win"),
+    }
 }
