975bdd812dcc27dc591b725125488f7e9c74ff39
JohnII7 <157798378@qq.com>
2022-07-17 18:37:00 +0800

    rust gen_range函数

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::io;
-
+use std::io; //prelude
+use rand::Rng; //trait
 fn main() {
     println!("猜个数字");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("神秘数字是{}",secret_number);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("无法执行");
     println!("你猜的是: {}",guess);
