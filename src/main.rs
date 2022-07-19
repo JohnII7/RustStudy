ec1bcc96ad87878298b946d6b84ecb61c0b139e4
JohnII7 <157798378@qq.com>
2022-07-19 11:36:52 +0800

    rust-变量可变性

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,20 @@ use std::io;
 //prelude
 use rand::Rng;
 
-//trait
 fn main() {
-    println!("猜个数字");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("无法执行");
+    // 变量不可变性, 需要加上mut才能变化
+    println!("Hello, World");
+    let mut x = 5;
+    println!("x:{}", x);
+    x = 6;
+    println!("x:{}", x);
 
-        // shadow
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
+    // shadowing隐藏同名变量
+    let y = 5;
+    let y = y + 1;
+    println!("the value of y is {}",y);
 
-        println!("你猜的是: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small !"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
-        }
-    }
+    let space = "    ";
+    let space = space.len();
+    println!("space len = {}",space);
 }
